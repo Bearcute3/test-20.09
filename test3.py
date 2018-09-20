ef8a32593e67457f49f314dea7aea5a27e69e183
Bearcute3 <35570449+Bearcute3@users.noreply.github.com>
2018-09-20 11:14:51 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,16 @@
-
+number = int(input())
+numbercopy = number
+i = 2
+ans = ""
+long = 0
+while number != 1:
+    if number % i == 0:
+        number = number // i
+        ans = ans + str(i) + "*"
+    if number % i != 0:
+        i += 1
+if numbercopy == 1:
+    print(number, "=", "1")
+else:
+    ans = ans[:-1]
+    print(numbercopy, "=", ans)
